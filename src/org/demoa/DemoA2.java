41b71fb19e17ab8b2cc031120958a1ea840bde48
Vinithkumar1997 <vinithsivakumar14@gmail.com>
2021-02-05 00:30:28 +0530

    updated from c

--- src/org/demoa/DemoA2.java
+++ src/org/demoa/DemoA2.java
@@ -4,5 +4,6 @@ public class DemoA2 {
 	public static void main(String[] args) {
 		System.out.println("DemoA2");
 		System.out.println("Demo A2 updated from B Branch");
+		System.out.println("Demo A2 updated from C branch");
 	}
 }
