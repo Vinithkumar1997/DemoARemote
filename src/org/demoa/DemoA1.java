2ed4b7f23d41072c6336a2dc910ccf211ec0d04e
Vinithkumar1997 <vinithsivakumar14@gmail.com>
2021-02-05 16:22:05 +0530

    updated now C Branch

--- src/org/demoa/DemoA1.java
+++ src/org/demoa/DemoA1.java
@@ -4,10 +4,7 @@ public class DemoA1 {
 	public static void main(String[] args) {
 		System.out.println("DemoA1");
 		System.out.println("DemoA1 is updated");
-<<<<<<< HEAD
-		System.out.println("DemoA1 is updated from C Branch");
-=======
 		System.out.println("DemoA1 is updated from B branch");
->>>>>>> 6c71ecb4e69fa80515b806abe0b50cc934164335
+		System.out.println("DemoA1 is updated from C Branch");
 	}
 }
